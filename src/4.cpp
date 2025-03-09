ae461512d2ad90b52d4b8c7b01d4d04c74040468
minhasnuman309 <minhasnuman309@gmail.com>
2025-03-09 20:03:01 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,11 @@
+// This is a comment
+int main() {
+  // Declare variables
+  int age = 20;
+  double gpa = 3.5;
+  
+  // Print output to console
+  cout << "My name is Sarah and I am " << age << " years old." << endl;
+  cout << "My GPA is " << gpa << "." << endl;
+  return 0;
+}
