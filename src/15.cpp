84637947ccf16c5b650f36dd9252505e24f9517b
minhasnuman309 <minhasnuman309@gmail.com>
2025-03-20 17:40:55 +0000

    Add 15.cpp

---
+++ src/15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    if (age >= 18) {
+        cout << "You are old enough to attend this class." << endl;
+    } else {
+        cout << "Sorry, you cannot attend this class." << endl;
+    }
+    return 0;
+}
