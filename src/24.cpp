15a2f07f9e3afa15e139feb3030b3e98a25fd1a2
minhasnuman309 <minhasnuman309@gmail.com>
2025-04-05 05:05:49 +0000

    Add 24.cpp

---
+++ src/24.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+    char z = 'A';
+    
+    cout << "x is " << x << endl;
+    cout << "y is " << y << endl;
+    cout << "z is " << z << endl;
+    
+    return 0;
+}
