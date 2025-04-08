489549db6d80a10985ab8dd3078fb4081d3d9431
minhasnuman309 <minhasnuman309@gmail.com>
2025-04-08 10:22:36 +0000

    Add 27.cpp

---
+++ src/27.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+
+    // Output the values to the console
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+
+    return 0;
+}
