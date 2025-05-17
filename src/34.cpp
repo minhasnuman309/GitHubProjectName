0bda360b6a949d4b7567671982e8b27f649dace7
minhasnuman309 <minhasnuman309@gmail.com>
2025-05-17 03:41:13 +0000

    Add 34.cpp

---
+++ src/34.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    if (age >= 18) {
+        cout << "You are old enough to vote." << endl;
+    } else {
+        cout << "You are not old enough to vote." << endl;
+    }
+    return 0;
+}
