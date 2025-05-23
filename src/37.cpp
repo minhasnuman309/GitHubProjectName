69d2b01e43ce5f71f8fa1afbeceed0839173e96d
minhasnuman309 <minhasnuman309@gmail.com>
2025-05-23 04:44:17 +0000

    Add 37.cpp

---
+++ src/37.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    std::cout << "x is " << x << std::endl;
+
+    if (x > 10) {
+        std::cout << "x is greater than 10" << std::endl;
+    } else {
+        std::cout << "x is not greater than 10" << std::endl;
+    }
+
+    return 0;
+}
